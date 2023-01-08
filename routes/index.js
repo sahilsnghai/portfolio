754c4d23d2dce02a831a7ada968d7f5106edae7e
Sahilsnghai <Sahilsinghai5672@gmail.com>
2023-01-08 11:28:13 +0530

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,50 @@
 const e = require('express');
 var express = require('express');
 var router = express.Router();
-const nodemailer =require("nodemailer");
+const nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('', function (req, res) {
   res.render('index');
 });
 
 /* POST contact page. */
-router.post('/forms', function(req, res, next) {
-    const {name,email,subject,message} = req.body;
-    try {
-      const transporter = nodemailer.createTransport(smtpTransport({
-        service:"gmail",
-        auth:{
-          user:process.env.EMAIL,
-          pass:process.env.PASSWORD
-        }
-      }))
-      const emailOptions = {
-        from: process.env.EMAIL, // sender address
-        to: email, // list of receivers
-        subject: subject, // Subject line
-        // text: "laude", // plain text body
-        html: `<h4 style="font:'montserrat';">Message from ${name}</h4><br><h2>Message: ${message}</h2><br><h5>Email: ${email}</h5>`, // html body
+router.post('', function (req, res, next) {
+  const { name, email, subject, message } = req.body;
+  try {
+    const transporter = nodemailer.createTransport(smtpTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
       }
-      transporter.sendMail(emailOptions, (error,info)=>{
-        if (error){
-          console.log(error)
-        } else{
-          console.log(info.res);
-          res.render("index")
-          // res.status(201).json({status:201,info})
-        }
-        
-      })
-    } catch (error) {
-      res.status(201).json({status:401,error})
-      
+    }))
+    const emailOptions = {
+      from: process.env.EMAIL, // sender address
+      to: 'sahilsinghai5672@gmail.com', // list of receivers
+      subject: subject, // Subject line
+      // text: "laude", // plain text body
+      html: `<h4 style="font:'montserrat';">
+        Message from ${name}</h4><br><h2>Message: ${message}
+        </h2><br><h5>Email: ${email}</h5>`, // html body
     }
-    // console.log(req.body)
-    // console.log(req.body)
-    // res.render('index');
-  });
-  
-  module.exports = router;
-  
+    transporter.sendMail(emailOptions, (error, info) => {
+      if (error) {
+        console.log('error1:',error)
+      } else {
+        console.log('error2:',info.response);
+        res.render("index.ejs")
+        // res.status(201).json({status:201,info})
+      }
+
+    })
+  } catch (error) {
+    res.status(201).json({ status: 401, error })
+
+  }
+  // console.log(req.body)
+  // console.log(req.body)
+  // res.render('index');
+});
+
+module.exports = router;
